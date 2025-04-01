129fa32bd255f8272b15fafe229e39ffb01fd61f
Damian Bugaj <damianbugajit@gmail.com>
2025-04-01 21:34:09 +0200

    Add public lesson list view and template

---
+++ lessons/urls.py
@@ -0,0 +1,8 @@
+# lessons/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.lesson_list, name='lesson_list'),
+]
--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Lesson
+
+def lesson_list(request):
+    lessons = Lesson.objects.all()
+    return render(request, 'lessons/lesson_list.html', {'lessons': lessons})
