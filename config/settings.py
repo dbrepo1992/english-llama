f7b9718ecb766f4e104b03117b1a407ee0d16c1c
Damian Bugaj <damianbugajit@gmail.com>
2025-04-02 18:03:26 +0200

    Add profile view and template for user dashboard

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# or '/games/memory/' or any custom view
+
+LOGIN_REDIRECT_URL = '/lessons/'  # or '/games/memory/' or any custom view
